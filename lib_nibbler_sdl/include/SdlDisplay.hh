a3519ca10e284ed00710f3515dced4f7c4266995
laguet_p <laguet_p@epitech.eu>
2014-04-01 22:10:04 +0200

    pm15

--- lib_nibbler_sdl/include/SdlDisplay.hh
+++ lib_nibbler_sdl/include/SdlDisplay.hh
@@ -20,10 +20,14 @@ public:
 
 private:
   SDL_Surface		*_screen;
+  //  SDL_Surface		*_screenStart;
   SDL_Surface		*_backgroundDisplay;
   SDL_Surface		*_snakeDisplay;
 
-  void			init(const GameBoard & game); // malloc & load bmp
+  void			init(const GameBoard & game);
+  void			initWindow(const GameBoard & game);
+  void			initBackground(const GameBoard & game);
+  void			initSnake(const GameBoard & game);
   void			getKey() const;
   void			update(const GameBoard & game); // refresh full
   void			refreshBg();
--- lib_nibbler_sdl/src/SdlDisplay.cpp
+++ lib_nibbler_sdl/src/SdlDisplay.cpp
@@ -5,7 +5,7 @@
 // Login   <chauvo_t@epitech.net>
 //
 // Started on  Tue Apr  1 21:22:02 2014 chauvo_t
-// Last update Tue Apr  1 21:12:36 2014 
+// Last update Tue Apr  1 22:07:34 2014 
 //
 
 # include "../include/SdlDisplay.hh"
@@ -25,7 +25,9 @@ extern "C"
 
 void		SdlDisplay::close()
 {
-
+  SDL_FreeSurface(this->_snakeDisplay);
+  SDL_FreeSurface(this->_screen);
+  SDL_FreeSurface(/*Fruits, wall, div items*/);
 }
 
 void		SdlDisplay::update(const GameBoard & game)
@@ -38,7 +40,28 @@ void		SdlDisplay::getKey()
 
 }
 
-void		SdlDisplay::init(const GameBoard & game)
+void		SdlDisplay::initSnake(const GameBoard &game)
+{
+  this->_snakeDisplay = SDL_LoadBMP("sprit/bmp_sdl/snake.bmp");
+  if (this->_snakeDisplay == NULL)
+    throw Exception("[ERROR] : Snake failed LoadBMP");
+  SDL_SetColorKey(this->_snakeDisplay, SDL_SRCCOLORKEYC, SDL_MapRGB(this->_screen->format, 0, 0, 0));
+  SDL_BlitSurface(this->_snakeDisplay, NULL, this->_screen, game.snake();
+}
+
+void		SdlDisplay::initBackground(const GameBoard &game)
+{
+  SDL_Rect	pos;
+
+  pos.x = 0;
+  pos.y = 0;
+  this->_backgroundDisplay = SDL_LoadBMP("sprit/bmp_sdl/jungle.bmp");
+  if (this->_backgroundDisplay == NULL)
+    throw Exception("[ERROR] : Background failed LoadBMP");
+  SDL_BlitSurface(this->_backgroundDisplay, NULL, this->_screen, &pos);
+}
+
+void		SdlDisplay::initWindow(const GameBoard & game)
 {
   if (SDL_Init(SDL_INIT_VIDEO) != 0)
     throw Exception("[ERROR] : SDL_INIT_VIDEO failled");
@@ -49,4 +72,12 @@ void		SdlDisplay::init(const GameBoard & game)
   SDL_WM_SetCaption("Nibbler (SDL)" , NULL);
   SDL_FillRect(this->_screen, NULL, SDL_MapRGB(this->_screen->format, 0, 0, 0));
   SDL_Flip(this->_screen);
+}
+
+
+void		SdlDisplay::init(const GameBoard & game)
+{
+  initWindow(&game);
+  initBackground(&game);
+  initSnake(&game);
 }
