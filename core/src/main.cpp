7d7fdb6423a0453df1c4cb7ac05a8615c28f7f40
chauvo_t <deb0ch@bitbucket.org>
2014-04-01 23:51:46 +0200

    correction de l'exemple sur la gestion d'erreur

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 #include <dlfcn.h>
-#include "IAssistant.hh" //Include only the interface
+#include "../../interface/IAssistant.hh" //Include only the interface
 
 int main(int ac, char** av)
 {
-	if (ac < 2)
-	{
-		std::cerr << "Usage : ./exemple4 <libXXX.so>" << std::endl;
-		return(1);
-	}
+  if (ac < 2)
+    {
+      std::cerr << "Usage : ./exemple4 <libXXX.so>" << std::endl;
+      return(1);
+    }
 
-	IAssistant* (*external_creator)();
-	void* dlhandle;
+  IAssistant* (*external_creator)();
+  void* dlhandle;
+  char	*error;
 
-	dlhandle = dlopen(av[1], RTLD_LAZY);
-	if (dlhandle == NULL)
-		return(1);
+  dlhandle = dlopen(av[1], RTLD_LAZY);
+  if (dlhandle == NULL)
+    return(1);
 
-	external_creator = reinterpret_cast<IAssistant* (*)()>(dlsym(dlhandle, "create_assistant"));
-	if (external_creator == NULL)
-		return(1);
+  dlerror();
+  external_creator = reinterpret_cast<IAssistant* (*)()>(dlsym(dlhandle, "create_assistant"));
+  if ((error = dlerror()) != NULL)
+    {
+      std::cerr << error << std::endl;
+      return(1);
+    }
 
-	IAssistant* bob = external_creator(); //Object included from the library !
+  IAssistant* bob = external_creator(); //Object included from the library !
 
-	bob->talk(); //Call the code of an unknown object from the code !
+  bob->talk(); //Call the code of an unknown object from the code !
 
-	dlclose(dlhandle);
+  dlclose(dlhandle);
 
-	return (0);
-	
+  return (0);
 }
