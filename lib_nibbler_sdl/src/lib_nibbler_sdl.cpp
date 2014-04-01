38991fe9bf6cf75cc21a2f4f1ad1d0bd9a89702b
laguet_p <laguet_p@epitech.eu>
2014-04-01 19:41:30 +0200

    pm11

--- lib_nibbler_sdl/src/lib_nibbler_sdl.cpp
+++ lib_nibbler_sdl/src/lib_nibbler_sdl.cpp
@@ -5,7 +5,7 @@
 // Login   <chauvo_t@epitech.net>
 //
 // Started on  Tue Apr  1 21:22:02 2014 chauvo_t
-// Last update Tue Apr  1 19:35:43 2014 
+// Last update Tue Apr  1 19:41:08 2014 
 //
 
 #include "../include/sdl.hh"
@@ -35,5 +35,11 @@ void		sdl::sdl_main_loop()
 
 void		sdl::sdl_init()
 {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    std::cout << "[ERROR] : SDL_INIT_VIDEO failled" << std::endl;
 
+  //init le screen ?????
+
+  SDL_WM_SetCaption("Nibbler (SDL)" , NULL);
+  SDL_FillRect(SDL_Surface *dst/*screen*/, NULL, Uint32 color)
 }
