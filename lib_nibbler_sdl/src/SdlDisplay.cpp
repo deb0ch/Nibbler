95fbfbd571a6aae994d7cef92c7fdf5cead7bdba
laguet_p <laguet_p@epitech.eu>
2014-04-01 21:12:57 +0200

    pm15

--- lib_nibbler_sdl/src/SdlDisplay.cpp
+++ lib_nibbler_sdl/src/SdlDisplay.cpp
@@ -5,7 +5,7 @@
 // Login   <chauvo_t@epitech.net>
 //
 // Started on  Tue Apr  1 21:22:02 2014 chauvo_t
-// Last update Tue Apr  1 21:08:27 2014 
+// Last update Tue Apr  1 21:12:36 2014 
 //
 
 # include "../include/SdlDisplay.hh"
@@ -41,7 +41,7 @@ void		SdlDisplay::getKey()
 void		SdlDisplay::init(const GameBoard & game)
 {
   if (SDL_Init(SDL_INIT_VIDEO) != 0)
-    std::cout << "[ERROR] : SDL_INIT_VIDEO failled" << std::endl;
+    throw Exception("[ERROR] : SDL_INIT_VIDEO failled");
 
   this->_screen = SDL_SetVideoMode(NB_PIX_X * game.width(), NB_PIX_Y * game.height(),
 				   COLOR, SDL_HWSURFACE);
