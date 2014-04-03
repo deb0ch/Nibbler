600ad34db8cd5e711e7992b7a846db06ca38eab1
chauvo_t <deb0ch@bitbucket.org>
2014-04-03 17:03:49 +0200

    Blablablablabla, c'est un véritable chat ici.

--- core/include/Core.hh
+++ core/include/Core.hh
@@ -5,7 +5,7 @@
 // Login   <chauvo_t@epitech.net>
 //
 // Started on  Wed Apr  2 17:25:33 2014 chauvo_t
-// Last update Thu Apr  3 16:27:09 2014 chauvo_t
+// Last update Thu Apr  3 17:03:10 2014 chauvo_t
 //
 
 #ifndef		CORE_H_
@@ -16,11 +16,14 @@
 # include "../include/Exception.hh"
 # include "../include/Timer.hh"
 # include "../../interface/GameBoard.hh"
+# include "../../interface/IDisplay.hh"
 
 class Core
 {
 public:
   void			startGame(int height, int width, const std::vector<std::string> & libs);
+  void			switchLib();
+  void			endGame();
 
 public:
   Core();
@@ -31,8 +34,15 @@ private:
   Core &operator=(const Core &);
 
 private:
-  Timer			_timer;
-  GameBoard		_game;
+  Timer					_timer;
+  GameBoard				_gameBoard;
+  const std::vector<std::string>	_libs;
+  std::vector<std::string>::iterator	_libsIt;
+  void					*_libHandle;
+  void					(*_init)(const GameBoard & game);
+  void					(*_update)(const GameBoard & game);
+  IDisplay::eKey			(*_getKey)();
+  void					(*_close)();
 
 private:
   void			openLib();
