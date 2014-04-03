5f3fcf9113c6b02fe1573cee48ee22cf68f86605
chauvo_t <deb0ch@bitbucket.org>
2014-04-03 17:29:02 +0200

    lol

--- core/src/GameBoard.cpp
+++ core/src/GameBoard.cpp
@@ -5,7 +5,35 @@
 // Login   <chauvo_t@epitech.net>
 //
 // Started on  Wed Apr  2 14:12:21 2014 chauvo_t
-// Last update Wed Apr  2 14:25:45 2014 chauvo_t
+// Last update Thu Apr  3 17:28:26 2014 chauvo_t
 //
 
-#include "../../interface/Game.hh"
+#include "../../interface/GameBoard.hh"
+
+// Public
+
+int	GameBoard::getWidth() const
+{
+  return (this->_width);
+}
+
+int	GameBoard::getHeight() const
+{
+  return (this->_height);
+}
+
+void	GameBoard::setWidth(int width)
+{
+  this->_width = width;
+}
+
+void	GameBoard::setHeight(int height)
+{
+  this->_height = height;
+}
+
+GameBoard::GameBoard() {}
+
+GameBoard::~GameBoard() {}
+
+// Private
