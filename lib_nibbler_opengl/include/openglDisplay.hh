f197a80ce8200a1fe18a26420c6a3bae62426fba
maxbou <bourgeois.maxim@gmail.com>
2014-04-05 23:00:41 +0200

    Opengl lib   NON teste

---
+++ lib_nibbler_opengl/include/openglDisplay.hh
@@ -0,0 +1,61 @@
+//
+// openglDisplay.hh for nibbler in /home/max/e-wip/cpp_nibbler
+//
+// Made by bourge_i
+// Login   <max@epitech.net>
+//
+// Started on  Sat Apr  5 21:20:19 2014 bourge_i
+// Last update Sat Apr  5 22:45:19 2014 bourge_i
+//
+
+#ifndef		OPENGLDISPLAY_H_
+# define	OPENGLDISPLAY_H_
+
+# define	BLOCKSIZE 0.05
+
+# include <GL/gl.h>
+# include <GL/glut.h>
+# include <GL/freeglut_std.h>
+# include <GL/glu.h>
+
+# include "../../interface/IDisplay.hh"
+# include "../../interface/GameBoard.hh"
+# include "../../interface/AItem.hh"
+# include "../../interface/Fruit.hh"
+# include "../../interface/Exception.hh"
+# include "../../interface/SnakeRing.hh"
+
+class OpenglDisplay : public IDisplay
+{
+public:
+  virtual void			init(const GameBoard & game);
+  virtual int			getFps() const;
+  virtual void			update(const GameBoard & game);
+  virtual IDisplay::eKey	getKey();
+  virtual void			close() const;
+
+public:
+  OpenglDisplay();
+  virtual ~OpenglDisplay() {}
+
+private:
+  // void	display();
+  // void	square(double x, double y, double offsetX, double offsetY,
+  // 	       int r, int v, int b); // todo creer structure
+  // void	manageKeyboard(unsigned char key, int x, int y);
+  // void	manageDirection(int key, int x, int y);
+  // void	timerCallback(int value);
+
+private:
+  OpenglDisplay(const OpenglDisplay &);
+  OpenglDisplay &operator=(const OpenglDisplay &);
+
+public:
+  double		myoffX;
+  double		myoffY;
+  int			direction;
+  IDisplay::eKey	key;
+
+};
+
+#endif /* !OPENGLDISPLAY_H_ */
