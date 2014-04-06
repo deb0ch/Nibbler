0b24d051b0360ce2863a4f26ad506f1dce4139bb
chauvo_t <deb0ch@bitbucket.org>
2014-04-06 22:51:01 +0200

    pour pull

--- lib_nibbler_ncurses/src/NcursesDisplay.cpp
+++ lib_nibbler_ncurses/src/NcursesDisplay.cpp
@@ -5,7 +5,7 @@
 // Login   <chauvo_t@epitech.net>
 //
 // Started on  Sat Apr  5 20:32:14 2014 chauvo_t
-// Last update Sun Apr  6 22:35:35 2014 chauvo_t
+// Last update Sun Apr  6 22:50:14 2014 chauvo_t
 //
 
 #include "../include/NcursesDisplay.hh"
@@ -99,7 +99,7 @@ IDisplay::eKey	NcursesDisplay::getKey()
 	output = IDisplay::NIB_KEY_RIGHT;
       else if (ch == ' ')
 	output = IDisplay::NIB_KEY_SPACE;
-      else if (ch == 27)
+      else if (ch == '\n')
 	output = IDisplay::NIB_KEY_ESC;
       else if (ch == KEY_ENTER)
 	output = IDisplay::NIB_KEY_ENTER;
